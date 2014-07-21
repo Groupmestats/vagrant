14d1e61e0444f7e4962896b6fd00f35eff4ae8ea
Brian Stascavage <bstascav@mog.home>
2014-07-20 22:33:14 -0400

    Initial commit

---
+++ modules/camping/manifests/init.pp
@@ -0,0 +1,29 @@
+class camping {
+
+    file {'/etc/default/camping-server':
+	source => '/vagrant/modules/camping/files/camping-server.default',
+	mode => 755,
+    }
+
+    file {'/etc/init.d/camping-server':
+        source => '/vagrant/modules/camping/files/camping-server.init',
+        mode => 755,
+    }
+
+    file {'/app/groupme_stats/camping/web.yaml':
+	source => '/vagrant/modules/camping/files/web.yaml',
+	mode => 744,
+    }
+
+    package { 'camping':
+        ensure => present,
+    }
+
+    service { 'camping-server':
+	ensure => running,
+	enable => true,
+	require => [ File['/etc/default/camping-server'],
+		     File['/etc/init.d/camping-server'],
+		     Package['camping'] ],
+    }
+}
